4adedd72041f6a8e33b5919fa53bdca1ff903f89
Kashish Desai <kashishdesai1997@gmail.com>
2021-02-13 15:54:31 -0800

    Assignment 1 - InstaPost App - Server Protocol

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+// Import useState and useEffect hooks from React
+import React, { useState, useEffect } from "react";
+
+// Import the API category from AWS Amplify
+import { API } from "aws-amplify";
+import "./App.css";
 
 function App() {
+  // Create service calls variable and set to empty String
+  const [servicecalls, updateServiceCalls] = useState([]);
+  const [nicknames, updateNicknames] = useState([]);
+  const [hashtags, updateHashtags] = useState([]);
+
+  // Define function to InstaPostAPI
+  async function fetchServiceCalls() {
+    const servicecalls = await API.get("instapostapi", "/servicecalls");
+    updateServiceCalls(servicecalls);
+  }
+  async function fetchNicknames() {
+    const nicknames = await API.get("instapostapi", "/nicknames");
+    updateNicknames(nicknames);
+  }
+  async function fetchHashtags() {
+    const hashtags = await API.get("instapostapi", "/hashtags");
+    updateHashtags(hashtags);
+  }
+
+  // Call fetchServiceCalls, fetchNicknames & fetchHashtags function when component loads
+  useEffect(() => {
+    fetchServiceCalls();
+    fetchNicknames();
+    fetchHashtags();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>InstaPost Application</h1>
+      <div>
+        <h2>Service Calls</h2>
+        <h5>{servicecalls}</h5>
+        <h2>Nicknames</h2>
+        {console.log(nicknames)}
+        <ol>
+          {nicknames.map((nickname) => (
+            <li key={nickname}>{nickname}</li>
+          ))}
+        </ol>
+        <h2>Hashtags</h2>
+        <ol>
+          {hashtags.map((hashtag) => (
+            <li key={hashtag}>{hashtag}</li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 }
